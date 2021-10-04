49bd3dd657fc35ebb5c8668e5aa4863d4c8a33da
phannguyenlong <phannguyenlong0812@gmail.com>
2021-10-04 23:58:50 +0700

    server: add api for query by key

--- admin_dashboard/application/routes/ledgerRoutes.js
+++ admin_dashboard/application/routes/ledgerRoutes.js
@@ -1,6 +1,6 @@
 /**
  * File for ledger route (interact with ledger)
- * @author Lam Xuan Bach, Hieu
+ * @author Lam Xuan Bach, Vuong Chi Hieu, Phan Nguyen Long
  */
 const express = require("express")
 const router = express.Router()
@@ -9,6 +9,7 @@ const { Gateway } = require('fabric-network');
 const { createContract } = require('../util/WebUtil.js');
 const chaincodeName = "assembly_line"
 
+// GET /api/ledger/queryAll
 router.get('/queryAll', async function (req, res) {
     const gateway = new Gateway()
 
@@ -22,15 +23,34 @@ router.get('/queryAll', async function (req, res) {
         
     } catch (err) {
         console.error("error: " + err)
-        res.redirect('/login')
+        res.redirect('/login.html')
     } finally {
         gateway.disconnect()
     }
 })
 
+// GET /api/ledger/queryByKey
 router.get("/queryByKey", async function (req, res) {
+    const gateway = new Gateway()
     let key = req.query.id // get param from request
-    console.log(key)
+    let queryString = {
+        selector: {_id: key} // put the key here to query
+    }
+    console.log(queryString)
+    try {
+        // get contract from the network
+        const contract = await createContract(gateway, chaincodeName, req.cookies.session)
+
+        console.log("GET Asset by key")
+        let data = await contract.evaluateTransaction('QuerryProduct', JSON.stringify(queryString)) // remember to convert to stirng pass
+        res.status(200).json(JSON.parse(data.toString()))
+        
+    } catch (err) {
+        console.error("error: " + err)
+        res.send(500)
+    } finally {
+        gateway.disconnect()
+    }
 })
 
 module.exports = router
