943eaade9817dda307ccf82d37da512bba9e26db
phannguyenlong <phannguyenlong0812@gmail.com>
2021-10-05 11:56:23 +0700

    fronted: add 404 page

--- admin_dashboard/application/server.js
+++ admin_dashboard/application/server.js
@@ -6,6 +6,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+const path = require('path');
 var app = express();
 
 // config app
@@ -20,6 +21,10 @@ app.use("/api/", apiRoutes)
 // for page
 app.use('/', express.static('pages'))
 
+// for 404 page
+app.use("*", function (req, res) {
+    res.sendFile(path.join(__dirname, '/pages/404.html'));
+})
 
 console.log("Server is listnening at port: 8080")
 app.listen(8080)
