817cb8ad69440033f3df5017c9cf1c765a98bad7
bipowerhcmcity <lmht772000@gmail.com>
2021-10-06 17:47:57 +0700

    upload VZ API

--- admin_dashboard/application/routes/networkRoutes.js
+++ admin_dashboard/application/routes/networkRoutes.js
@@ -2,16 +2,43 @@
  * File for network route for server (interact with the network)
  * @author Truong Minh Khoa, Le Vinh Nguyen
  */
-const express = require("express")
-const router = express.Router()
 
-router.get('/', async function (req, res) {
-    res.send("hello")
+const express = require("express");
+const router = express.Router();
+var glob = require("glob")
+const fs = require("fs")
+
+router.get('/getChannelStatus', async function (req, res) {
+
+    console.log("-----------------------")
+    console.log("Channel Statuses")
+    channelStatuses = []
+    namesChannel = glob.sync("../../test-network/log/channels/*")
+    var json;
+    console.log(namesChannel)
+    for (let j = 0; j < namesChannel.length; j++) {
+        file = namesChannel[j]
+        console.log(file)
+        var Status_channel = fs.readFileSync(file, 'utf8')
+        console.log(Status_channel); 
+        json = JSON.parse(Status_channel.substring(
+            Status_channel.indexOf("{"), Status_channel.lastIndexOf("}") + 1
+            ));  
+        delete json["url"]; 
+        delete json["consensusRelation"]; 
+        delete json["height"];
+        //console.log(json);
+        channelStatuses.push(json);
+    } 
+    console.log(channelStatuses)
+
+    res.json(channelStatuses);
+        
+    
 })
 
 router.get('/show_detail' ,async function (req, res) {
     console.log(req.query.channel)
-    const fs = require('fs')
 
     try {
     const data = fs.readFileSync('../../test-network/log/network_config.log', 'utf8').split("\n")
