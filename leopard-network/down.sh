e5ad53a57ddf82061a2e70baaa90366c5d072162
bipowerhcmcity <lmht772000@gmail.com>
2021-10-12 22:37:43 +0700

    Update down.sh

--- leopard-network/down.sh
+++ leopard-network/down.sh
@@ -4,8 +4,10 @@ for entry in `find docker -type f`; do
 done
 
 docker volume prune -f # remove all volumes
+docker rm -f $(docker ps -a -q)
 
 rm -r organizations/*
 rm -r docker/*
 rm -r channel-artifacts/*
 rm -r channel-config/*
+rm -r tmp/*
