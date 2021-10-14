575b1af7ddb53b7bc974760b180d8c198fc4a85a
bipowerhcmcity <lmht772000@gmail.com>
2021-10-14 14:07:55 +0700

    2 API

--- admin_dashboard/application/main.js
+++ admin_dashboard/application/main.js
@@ -24,6 +24,27 @@ async function main() {
     await createChannel(channel1)
     await deployCC('channel1',"admin_dashboard/chaincode/admin-chaincode")
 
+
+
+    let orderer2 = new OrdererOrganization("Company D", 'ordererAdmin', 'ordererPassword', 'admin', 'password', 'channel2', 8064)
+    let peers2 = [
+        new PeerOrganization("Company E", 'adminA', 'passwordA', 'peerAdminA', 'peerPasswordA', 'channel2', 6064),
+        new PeerOrganization("Company F", 'adminB', 'passwordB', 'peerAdminB', 'peerPasswordB', 'channel2', 7064)
+    ]
+    let channel2 = new Channel("channel2", orderer2, peers2)
+
+    // create orderer
+    await createOrdererAndCA(orderer2)
+
+    // create peer
+    for (let i = 0; i < peers2.length; i++) {
+        await creatPeerAndCA(peers2[i])
+    }
+
+    // join channel
+    await createChannel(channel2)
+    await deployCC('channel2',"admin_dashboard/chaincode/admin-chaincode")
+
 }
 
 main()
